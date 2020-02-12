a61ea88a658b1a1021d9f96165ea12a21060630f
Юрий Матыцин <ymatytsin@leads.team>
2020-02-12 15:43:59 +0300

    закончил слайдер

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,18 @@
 $(function(){
 
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px",
+    readOnly: true
+  });
+
+  $('.products-slider__inner').slick({
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    arrows: false,
+    dots: true
+  });
+
   var mixer = mixitup('.products__inner-box');
 
 });
