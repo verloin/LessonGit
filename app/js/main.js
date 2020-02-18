8482f9cfb07b80ceb03953dfcc211e242840e123
Юрий Матыцин <ymatytsin@leads.team>
2020-02-18 15:44:18 +0300

    сделал адаптив Header и Footer

--- app/js/main.js
+++ app/js/main.js
@@ -34,9 +34,16 @@ $(function(){
     $('.icon-th-list').removeClass('active');
   });
 
+  $('.menu__btn').on('click', function(){     // при нажатии появляется меню 
+    $('.menu__list').slideToggle();
+  });
+
+  $('.header__btn-menu').on('click', function(){     // при нажатии класс Active появляется при следующем нажатии - исчезает )) 
+    $('.header__box').toggleClass('active');
+  });
+
+  
 
   var mixer = mixitup('.products__inner-box');
 
 });
-
-icon-th-large
